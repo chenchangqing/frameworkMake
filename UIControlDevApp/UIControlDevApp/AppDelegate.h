6e4552c653be572e8575dbfd83574032b3578efc
chenchangqing <chenchangqing198@126.com>
2015-11-05 00:27:56 +0800

    .a文件制作成功

---
+++ UIControlDevApp/UIControlDevApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIControlDevApp
+//
+//  Created by green on 15/11/5.
+//  Copyright © 2015年 RayWenderlich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
